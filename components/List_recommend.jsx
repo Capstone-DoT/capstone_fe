8c90c919de12249fa535293135326f8cdd252ac8
jeongeunjii <wjddmsl000@naver.com>
2023-05-30 23:25:21 +0900

    1

--- components/List_recommend.jsx
+++ components/List_recommend.jsx
@@ -9,14 +9,17 @@ export default function list(props) {
     const [scholar, setScholar] = useState([])
     const [contest, setContest] = useState([])
     const [extra, setExtra] = useState([])
+    const [isLoading, setIsLoading] = useState()
     const { type } = props
     const { ordering } = props
 
     const getContents = async () => {
+        setIsLoading(true)
         const response = await contentapi(type, ordering)
         response.data.isSuccess && setScholar(response.data.result.AIResult.scholarship)
         response.data.isSuccess && setContest(response.data.result.AIResult.contest)
         response.data.isSuccess && setExtra(response.data.result.AIResult.activity)
+        setIsLoading(false)
     }
 
     const ScholarHandler = (e) => {
@@ -48,13 +51,26 @@ export default function list(props) {
         getContents()
     }, [type, ordering])
 
+
     return (
         <div className="pl-2">
+            {isLoading
+                ? <div role="status ">
+                    <svg aria-hidden="true" className="w-20  absolute top-72 left-[40%] mr-2 text-gray-light animate-spin fill-orange" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
+                </div>
+                : ((type === "scholarship" && scholar.length === 0) || (type === "contest" && contest.length === 0) || (type === "activity" && extra.length === 0)
+                    ? <div >
+                        <h1 className="text-[100px] absolute top-[350px] left-[40%]">😥</h1>
+                        <h1 className="absolute top-[500px] left-[30%] font-bold text-lg">해당 분야의 추천정보를 찾을 수 없습니다!</h1>
+                    </div>
+                    : <></>)
+            }
             {scholar.length !== 0 && scholar.map((content) => (
                 <button onClick={ScholarHandler} value={content.id} className="w-full text-left">
                     <div className="relative mt-3 w-full bg-gray/50 h-[1px]"></div>
                     <ul className='mb-4 text-black pl-1 '>
                         <li key={content.type} className="flex justify-start mt-6">
+                            <div className='flex justify-center items-center h-6 bg-gray/25 text-orange px-4 mr-1 text-center text-[11px] font-bold rounded-xl'>장학금</div>
                             <div className='flex justify-center items-center h-6 bg-gray/25 text-orange px-4 mr-1 text-center text-[11px] font-bold rounded-xl'>{content.type === "" ? "기타" : content.type}</div>
                         </li>
                         <li key={content.institution} className='mt-3 text-sub text-gray-dark'>{content.institution}</li>
@@ -68,6 +84,7 @@ export default function list(props) {
                     <div className="relative mt-3 w-full bg-gray/50 h-[1px]"></div>
                     <ul className='mb-4 text-black pl-1 '>
                         <li key={content.type} className="flex justify-start mt-6">
+                            <div className='flex justify-center items-center h-6 bg-gray/25 text-orange px-4 mr-1 text-center text-[11px] font-bold rounded-xl'>공모전</div>
                             <div className='flex justify-center items-center h-6 bg-gray/25 text-orange px-4 mr-1 text-center text-[11px] font-bold rounded-xl'>{content.type === "" ? "기타" : content.type}</div>
                         </li>
                         <li key={content.institution} className='mt-3 text-sub text-gray-dark'>{content.institution}</li>
@@ -81,6 +98,7 @@ export default function list(props) {
                     <div className="relative mt-3 w-full bg-gray/50 h-[1px]"></div>
                     <ul className='mb-4 text-black pl-1 '>
                         <li key={content.type} className="flex justify-start mt-6">
+                            <div className='flex justify-center items-center h-6 bg-gray/25 text-orange px-4 mr-1 text-center text-[11px] font-bold rounded-xl'>대외활동</div>
                             <div className='flex justify-center items-center h-6 bg-gray/25 text-orange px-4 mr-1 text-center text-[11px] font-bold rounded-xl'>{content.type === "" ? "기타" : content.type}</div>
                         </li>
                         <li key={content.institution} className='mt-3 text-sub text-gray-dark'>{content.institution}</li>
@@ -89,17 +107,6 @@ export default function list(props) {
                     </ul>
                 </button>
             ))}
-            {(scholar.length === 0 && contest.length === 0 && extra.length === 0) &&
-                <div role="status ">
-                    <svg aria-hidden="true" className="w-20  absolute top-72 left-[40%] mr-2 text-gray-light animate-spin fill-orange" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
-                </div>
-            }
-            {((type === "scholarship" && scholar.length === 0) || (type === "contest" && contest.length === 0) || (type === "activity" && extra.length === 0)) &&
-                <div >
-                    <h1 className="text-[100px] absolute top-[350px] left-[40%]">😥</h1>
-                    <h1 className="absolute top-[500px] left-[33%] font-bold text-lg">해당 분야의 추천정보를 찾을 수 없습니다!</h1>
-                </div>
-            }
         </div>
     )
 }
